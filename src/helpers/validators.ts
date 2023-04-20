12ffce55008e884407c73673c48145f76b3ae8a4
Harpreet Singh <90693057+devhs89@users.noreply.github.com>
2023-04-19 20:05:07 -0400

    FEATURE: add constant objects and validation helpers

---
+++ src/helpers/validators.ts
@@ -0,0 +1,12 @@
+// Validation Middleware
+export const emailValidator = (val) => {
+  return /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(val);
+};
+
+export const passwordValidator = (val) => {
+  return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(val);
+};
+
+export const phoneValidator = (val) => {
+  return /^\+\d{1}-\d{3}-\d{3}-\d{4}$/.test(val);
+};
